18c149e39e254bd0b6d32ed1161b20116142bca6
tdqc <tadeu123546@gmail.com>
2021-05-16 18:56:38 -0300

    Formatação da mensagem

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from static.objects.BotResponse import BotResponse
-from chatterbot.conversation import Statement, StatementMixin
 from chatbot import chatbot
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 import json
+import ftfy
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -15,10 +15,10 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')
     response = chatbot.get_response(userText)
-    objectResponse = BotResponse(response.confidence, response.text, response.in_response_to, response.conversation, response.tags)
+    formatedResponse = ftfy.fix_text(response.text)
+    objectResponse = BotResponse(response.confidence, formatedResponse, response.in_response_to, response.conversation, response.tags)
     jsonResponse = json.dumps(objectResponse.__dict__, ensure_ascii=False).encode('utf8')
     return jsonResponse 
-
 
 if __name__ == "__main__":
     app.run() 
