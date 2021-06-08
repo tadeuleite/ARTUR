f08b1be7369046a3f9eb2774bff8eeb014f7d747
Lucas Bessa <lucasbessa708@gmail.com>
2021-06-07 22:21:04 -0300

    modificaçao no layout do chatbot

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from typing import Text
+from chatterbot import conversation
+from chatterbot.conversation import Statement
 from static.objects.BotResponse import BotResponse
 from chatbot import chatbot
 from flask import Flask, render_template, request
@@ -15,6 +18,10 @@ def home():
 def get_bot_response():
     userText = request.args.get('msg')
     response = chatbot.get_response(userText)
+    previousState = chatbot.get_latest_response(response.conversation)
+    respondeGenerate = chatbot.generate_response(response)
+    chatbot.learn_response(response,  previousState)
+    chatbot.storage.create(text=respondeGenerate.text,in_response_to=respondeGenerate.in_response_to,conversation=respondeGenerate.conversation,persona=respondeGenerate.persona)
     formatedResponse = ftfy.fix_text(response.text)
     objectResponse = BotResponse(response.confidence, formatedResponse, response.in_response_to, response.conversation, response.tags)
     jsonResponse = json.dumps(objectResponse.__dict__, ensure_ascii=False).encode('utf8')
