cece3d622a68e2711ec4aba9cabba2ed442f4076
Lucas Bessa <lucasbessa708@gmail.com>
2021-06-29 22:24:46 -0300

    os.environ

--- service/appService.py
+++ service/appService.py
@@ -4,7 +4,7 @@ import psycopg2
 class appService(object): 
 
     def get_responses():
-        conn = psycopg2.connect('postgres://gtmzrjxctpfbwf:656cb219cfe588e047b012f19241c8431c223c27cf6e148a077f1ca63ccff875@ec2-34-230-115-172.compute-1.amazonaws.com:5432/dfcbi0ip98i74n', sslmode='require')
+        conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
         cur = conn.cursor()
         responses = []
         try:
